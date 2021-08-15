55408040dc05b61dbd97ccee4046c195070292d0
Stryder-Git <marcel_pieper@hotmail.com>
2021-08-15 15:41:46 -0400

    cleanup

--- main.py
+++ main.py
@@ -3,32 +3,3 @@ from Movati_SignUp import GUI, Dobby, Presenter
 gui = GUI(Presenter(), Dobby())
 gui.launch_main()
 
-
-# from threading import Thread, active_count
-# from time import sleep
-#
-# A = "no"
-#
-# def to_stop():
-#     for i in range(1, 102, 2):
-#         sleep(2)
-#         print(" *** ** * still running")
-#         if A.lower() == "yes":
-#             break
-#     print("-- stopped to_stop")
-#
-# print(active_count())
-# Thread(target= to_stop, daemon= True).start()
-# print(active_count())
-#
-# while A.lower() not in ("both", "this"):
-#     A = input("stop?:  ")
-#
-# print("------- broke input loop ---> :")
-# sleep(3)
-# print(active_count())
-#
-# # exit()
-
-
-
